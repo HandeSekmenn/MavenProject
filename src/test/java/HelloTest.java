264a40101b2edb5bf37556e17cd468409242f7f1
HANDE <handesekmen7@gmail.com>
2021-12-22 20:53:40 +0100

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,8 +2,10 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Test");
-
-
+        System.out.println("Developing hello feature");
+        System.out.println("Local Repo isalready published to GitHub");
+        System.out.println("Doing more work");
     }
 
 }
+//video 4te hata verdi.Şu an video 6dayım
