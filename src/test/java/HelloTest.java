201bbac5820fb99138d823b5199c16839c10f00b
HANDE <handesekmen7@gmail.com>
2021-12-22 23:02:42 +0100

    Fixed typo in the file

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,9 +3,11 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("Developing hello feature");
-        System.out.println("Local Repo isalready published to GitHub");
+        System.out.println("Local Repo is already published to GitHub");
         System.out.println("Doing more work");
     }
 
 }
-//video 4te hata verdi.Şu an video 6dayım
+//video 4te hata verdi
+//video 7de pull ve fetch kısmını yaptım ama master kısmında değişim olmadı adamda olduğu gibi
+//
